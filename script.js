d7c340f231c458487c395dde4744b2ced8029875
Katherine Ebel <kathyebel@me.com>
2021-11-12 10:43:27 -0500

    HTML Semantic improvements. Share style fixes

--- script.js
+++ script.js
@@ -2,9 +2,5 @@ const shareEl = document.querySelector('.share')
 const shareBtn = document.querySelector('.share-toggle-btn')
 
 shareBtn.addEventListener('click', () => {
-  if (shareEl.classList.contains('active')) {
-    shareEl.classList.remove('active')
-  } else {
-    shareEl.classList.add('active')
-  }
+  shareEl.classList.toggle('active')
 })
